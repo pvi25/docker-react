077ae814a4ddecb928bd9524c9ab252047fb3f20
root <root@localhost.localdomain>
2023-05-25 19:32:49 +0200

    commit feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there (deployed by github)!
+          Bye there (deployed by github)! Feature branch update
         </p>
         <a
           className="App-link"
