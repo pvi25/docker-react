03a8254d1ccae8512d6d69ac6e57fdd17a66a1ec
root <root@localhost.localdomain>
2023-05-25 07:49:26 +0200

    added github actions deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!
+          Bye there (deployed by github)!
         </p>
         <a
           className="App-link"
